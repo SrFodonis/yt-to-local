9c7cd9fc0b9bfe38e1e7b3229e55888a4067ca5f
Fodd <sebatron97@gmail.com>
2024-12-09 19:50:40 -0600

    working w/ good JSON, no classes

--- yt-to-local/json_from_ytdlp.py
+++ yt-to-local/json_from_ytdlp.py
@@ -26,36 +26,19 @@ with open("test_file.json", "w") as file:
     file.write(formatted_json)
 
 # Get playlist_control-style formatted file
-class playlist_simple:
-    def __init__(self, id:str, title:str, playlist_count:str, entries: list):
-        self.id = id
-        self.title = title
-        self.playlist_count = playlist_count
-        self.entries = entries
-        pass
-
-class playlist_entry:
-    def __init__(self, id:str, url: str, title:str, file_type:str):
-        self.id = id
-        self.url = url
-        self.title = title
-        self.file_type = file_type
-        pass
 
 videos = list()
 for entry in playlist_json["entries"]:
     videos.append(
-        playlist_entry(
-            id=entry["id"],
-            url=entry["url"],
-            title=entry["title"],
-            file_type="TBD"
-        )
+        {
+            "id": entry["id"],
+            "url": entry["url"],
+            "title": entry["title"],
+            "file_type": "TBD"
+        }
     )
 
-# Test dump
 with open("test_dump.json", "w") as file:
-    for video in videos:
-        file.write(
-            json.dumps(video.__dict__, indent=4)
-        )
+    file.write(
+        json.dumps(videos, indent=4)
+    )
